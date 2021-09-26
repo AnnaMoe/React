06bf4747b7086301d048ca2dfbb3be797f00efe2
Anna Moelling <anna.moelling@googlemail.com>
2021-09-26 18:17:39 +0200

    introducing state - by clicking button change value

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
@@ -5,15 +7,24 @@ import './ExpenseItem.css';
 // props allows you to pass data from another component to this component
 // props is key-value pairs here
 const ExpenseItem = (props) => {
-  
+ 
+  // react-hook:
+  // with this hook the props.title can be changed -- component function will be called again - assign a new value to this state
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Updated');
+    console.log(title);
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
   
